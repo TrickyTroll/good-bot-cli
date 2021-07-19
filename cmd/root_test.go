29f92a59495d9745c1e90d3fd8b6fba82be1bf4d
Etienne Parent <53831454+TrickyTroll@users.noreply.github.com>
2021-07-19 20:06:41 -0300

    Tests: now testing ` validatePath`

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -11,6 +11,24 @@ func TestGetDir(t *testing.T) {
 	splitDir := strings.Split(dir, "/")
 	stem := splitDir[len(splitDir)-1]
 	if !(stem == "cmd") {
-		t.Error(`getDir(%s)`, dir)
+		t.Errorf(`getDir(%s)`, dir)
+	}
+}
+
+func TestValidatePath(t *testing.T) {
+	var testCases = []struct {
+		input string
+		want  bool
+	}{
+		{"", false},
+		{"/", true},
+		{"/usr/", true},
+		{"/foobar/", false},
+		{"/usr/local", true},
+	}
+	for _, test := range testCases {
+		if got := validatePath(test.input); got != test.want {
+			t.Errorf("validatePath(%v) = %v, want %v", test.input, got, test.want)
+		}
 	}
 }
