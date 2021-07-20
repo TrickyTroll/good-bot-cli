733f2618657a6e80fc21cff3ce58fb42929945a2
Etienne Parent <53831454+TrickyTroll@users.noreply.github.com>
2021-07-20 15:45:39 -0300

    In progress: using a `TestMain` func

--- cmd/record_test.go
+++ cmd/record_test.go
@@ -1,21 +1,28 @@
 package cmd
 
 import (
+	"os"
 	"testing"
 )
 
+func TestMain(m *testing.M) {
+	setup()
+	testsRes := m.Run()
+	os.Exit(testsRes)
+}
+
 func TestIsDirectoryOnDir(t *testing.T) {
-	isDir := isDirectory(toCreate.testDir)
+	isDir := isDirectory(testData.dir)
 	if !isDir {
-		t.Errorf("isDirectory(%s) = %v", toCreate.testDir, isDir)
+		t.Errorf("isDirectory(%s) = %v", testData.dir, isDir)
 	}
 
 }
 
 func TestIsDirectoryOnFile(t *testing.T) {
-	isDir := isDirectory(toCreate.testFile)
+	isDir := isDirectory(testData.file)
 	if isDir {
-		t.Errorf("isDirectory(%s) = %v", toCreate.testDir, isDir)
+		t.Errorf("isDirectory(%s) = %v", testData.file, isDir)
 	}
 
 }
