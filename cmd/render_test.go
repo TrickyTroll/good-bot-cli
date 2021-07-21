d7db1d646cda722facbebcf0c1352546727b7220
Etienne Parent <53831454+TrickyTroll@users.noreply.github.com>
2021-07-20 22:05:52 -0400

    Tests: testing `getScenePath` errors

--- cmd/render_test.go
+++ cmd/render_test.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"os"
 	"path/filepath"
 	"strings"
 	"testing"
@@ -22,5 +23,21 @@ func TestGetScenePath(t *testing.T) {
 
 	if got != "scene_2" {
 		t.Errorf("getScenePath(%s) = %s, want %s", path, got, "scene_2")
+	}
+}
+
+func TestGetScenePathErr(t *testing.T) {
+	falsePath := "./foobar"
+	_, err := os.Stat(falsePath)
+
+	// File should not exist, there should be an error.
+	if err == nil {
+		t.Errorf("TestGetScenePathErr should be edited. Directory %s should not exist.", falsePath)
+	}
+
+	_, err = getScenePath(falsePath)
+
+	if err == nil {
+		t.Errorf("getScenePath(%s) should raise an error.", falsePath)
 	}
 }
