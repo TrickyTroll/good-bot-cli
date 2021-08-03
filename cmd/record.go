568891b2ed7c4561a4f3fdbf5cf84bad24852151
Etienne Parent <53831454+TrickyTroll@users.noreply.github.com>
2021-08-03 16:22:30 -0400

    Fix: add missing exit

--- cmd/record.go
+++ cmd/record.go
@@ -148,6 +148,7 @@ func runRecordCommand(hostPath string, ttsFile string, envVars []string, setting
 	if isRead && len(ttsFile) < 1 {
 		fmt.Println("You need a TTS credentials file to use 'read' statements in your script.")
 		fmt.Println("For more information on the credentials file, please refer to the documentation")
+		os.Exit(1)
 	}
 	inout := make(chan []byte)
 
